04f54771d951f9fd9f4e7cf9296492024ec52465
Philip Nnamdi Philip <philipandy2@gmail.com>
2020-03-26 17:34:36 +0100

    The Third Commit: More static files edited and added

--- my_app/views.py
+++ my_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render (request, template_name='base.html')
+
+def new_search(request):
+    return render (request, template_name='my_app/new_search.html')
